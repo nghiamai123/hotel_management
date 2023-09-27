e25221ecdf03b5fbab03c7c053f482dd2660d63f
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-09-27 12:02:10 +0700

    add file Detail.js

---
+++ js/Detail.js
@@ -0,0 +1,6 @@
+var showFormButton = document.getElementById("showFormButton");
+var myForm = document.getElementById("myForm");
+
+showFormButton.addEventListener("click", function() {
+  myForm.style.display = "block"; // Hiển thị form khi button được nhấp
+});
